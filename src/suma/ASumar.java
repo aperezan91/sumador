e41315fd3db18c6b6b01d7818569ec81986752db
apere <apere@DESKTOP-NRC1U9T>
2020-05-05 13:21:40 +0200

    prueba 1

--- src/suma/ASumar.java
+++ src/suma/ASumar.java
@@ -2,4 +2,16 @@ package suma;
 
 public class ASumar {
 
+
+	public ASumar() {
+		
+	}
+	public String mostrar(String cad) {
+        String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+		return numero; 
+
+	}
 }
--- test/suma/TestASumar.java
+++ test/suma/TestASumar.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 class TestASumar {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.mostrar("5");
+        assertEquals("5 = 5",resultado );
+    }
 
 }
