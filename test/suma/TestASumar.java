0d8470c53d48c82a261562e871ed5ba1628ae47d
apere <apere@DESKTOP-NRC1U9T>
2020-05-05 13:02:49 +0200

    commit inicial

---
+++ test/suma/TestASumar.java
@@ -0,0 +1,14 @@
+package suma;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestASumar {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
