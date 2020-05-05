649e0f2937199f339b12518072cdf57046d1ddd6
apere <apere@DESKTOP-NRC1U9T>
2020-05-05 13:24:12 +0200

    javadoc

--- src/suma/ASumar.java
+++ src/suma/ASumar.java
@@ -6,6 +6,14 @@ public class ASumar {
 	public ASumar() {
 		
 	}
+	
+	/**
+	 * Este metodo comprueba si el numero introducido es de un digito 
+	 * y devuelve una cadena
+	 * @author Adrian Perez
+	 * @param cad
+	 * @return numero
+	 */
 	public String mostrar(String cad) {
         String numero = cad;
         if (numero.length() == 1) {
